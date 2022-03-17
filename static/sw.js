b88aea965eb877c278ba5dd1086eb7533db239c1
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2022-03-17 07:27:45 -0400

    Bump cache version

--- static/sw.js
+++ static/sw.js
@@ -12,7 +12,7 @@ Thanks to [Jeremy][1], [Filament Group][2], and [Erik][3] for this, and everythi
 (function(){
   'use strict';
 
-  const version = 'v2020-10-15T17:02:00::';
+  const version = 'v2022-03-17T07:25:00::';
   const staticCacheName = version + 'static';
   const pagesCacheName = version + 'pages';
   const imagesCacheName = version + 'images';
